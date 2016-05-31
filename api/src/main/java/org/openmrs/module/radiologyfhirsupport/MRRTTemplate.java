5c6647e19d6f3f66b0fcda1abb0214c0252ab88a
maany <imptodefeat@gmail.com>
2016-05-31 23:03:21 +0530

    create liquibase.xml + update MRRTTemplate to be compatible with OpenMRS <1.11.6 due to TRUNK-4841

--- api/src/main/java/org/openmrs/module/radiologyfhirsupport/MRRTTemplate.java
+++ api/src/main/java/org/openmrs/module/radiologyfhirsupport/MRRTTemplate.java
@@ -15,9 +15,12 @@ package org.openmrs.module.radiologyfhirsupport;
 
 import java.io.Serializable;
 import java.sql.Clob;
+import java.util.Date;
 
+import org.openmrs.BaseOpenmrsData;
 import org.openmrs.BaseOpenmrsObject;
 import org.openmrs.BaseOpenmrsMetadata;
+import org.openmrs.User;
 
 import javax.persistence.*;
 
@@ -36,7 +39,37 @@ public class MRRTTemplate extends BaseOpenmrsObject implements Serializable {
 	@Lob
 	@Column(name = "xml")
 	private Clob xml;
-	
+	/*
+	* The fields below are added to support OpenMRS versions <1.11.6 where Hibernate Configuration for this class would fail because of TRUNK-4841
+	 */
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "creator")
+	protected User creator;
+
+	@Column(name = "date_created", nullable = false)
+	private Date dateCreated;
+
+	@ManyToOne
+	@JoinColumn(name = "changed_by")
+	private User changedBy;
+
+	@Column(name = "date_changed")
+	private Date dateChanged;
+
+	@Column(name = "voided", nullable = false)
+	private Boolean voided = Boolean.FALSE;
+
+	@Column(name = "date_voided")
+	private Date dateVoided;
+
+	@ManyToOne
+	@JoinColumn(name = "voided_by")
+	private User voidedBy;
+
+	@Column(name = "void_reason", length = 255)
+	private String voidReason;
+
 	@Override
 	public Integer getId() {
 		return id;
@@ -46,6 +79,7 @@ public class MRRTTemplate extends BaseOpenmrsObject implements Serializable {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Clob getXml() {
 		return xml;
 	}
@@ -53,5 +87,68 @@ public class MRRTTemplate extends BaseOpenmrsObject implements Serializable {
 	public void setXml(Clob xml) {
 		this.xml = xml;
 	}
-	
+
+	public User getCreator() {
+		return creator;
+	}
+
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public User getChangedBy() {
+		return changedBy;
+	}
+
+	public void setChangedBy(User changedBy) {
+		this.changedBy = changedBy;
+	}
+
+	public Date getDateChanged() {
+		return dateChanged;
+	}
+
+	public void setDateChanged(Date dateChanged) {
+		this.dateChanged = dateChanged;
+	}
+
+	public Boolean getVoided() {
+		return voided;
+	}
+
+	public void setVoided(Boolean voided) {
+		this.voided = voided;
+	}
+
+	public Date getDateVoided() {
+		return dateVoided;
+	}
+
+	public void setDateVoided(Date dateVoided) {
+		this.dateVoided = dateVoided;
+	}
+
+	public User getVoidedBy() {
+		return voidedBy;
+	}
+
+	public void setVoidedBy(User voidedBy) {
+		this.voidedBy = voidedBy;
+	}
+
+	public String getVoidReason() {
+		return voidReason;
+	}
+
+	public void setVoidReason(String voidReason) {
+		this.voidReason = voidReason;
+	}
 }
