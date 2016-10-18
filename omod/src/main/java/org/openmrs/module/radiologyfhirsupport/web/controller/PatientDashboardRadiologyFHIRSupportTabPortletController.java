2dedd8219ef96719b2369da5e0f147219dbd23e0
maany <imptodefeat@gmail.com>
2016-10-18 22:26:43 +0530

    fix FHIR module dependency issue i.e APIException: Service Not Found.https://talk.openmrs.org/t/module-development-org-openmrs-api-apiexception-service-not-found/1780/10

--- omod/src/main/java/org/openmrs/module/radiologyfhirsupport/web/controller/PatientDashboardRadiologyFHIRSupportTabPortletController.java
+++ omod/src/main/java/org/openmrs/module/radiologyfhirsupport/web/controller/PatientDashboardRadiologyFHIRSupportTabPortletController.java
@@ -1,10 +1,14 @@
 package org.openmrs.module.radiologyfhirsupport.web.controller;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.radiologyfhirsupport.MRRTTemplate;
+import org.openmrs.module.radiologyfhirsupport.api.MRRTTemplateService;
 import org.openmrs.web.controller.PortletController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,8 +18,14 @@ import java.util.Map;
 @RequestMapping(PatientDashboardRadiologyFHIRSupportTabPortletController.PATIENT_DASHBOARD_PORTLET_CONTROLLER)
 public class PatientDashboardRadiologyFHIRSupportTabPortletController extends PortletController {
     public static final String PATIENT_DASHBOARD_PORTLET_CONTROLLER = "**/patientDashboardRadiologyFHIRSupportTab.portlet";
+    private MRRTTemplateService mrrtTemplateService=null;
     @Override
     protected void populateModel(HttpServletRequest request, Map<String, Object> model) {
-        model.put("message","Yo ho Yo ho a pirates life for me!");
+        if(mrrtTemplateService==null)
+            mrrtTemplateService = Context.getService(MRRTTemplateService.class);
+        List<MRRTTemplate> mrrtTemplates = mrrtTemplateService.getAll();
+        model.put("availableTemplates",mrrtTemplates);
+
+
     }
 }
