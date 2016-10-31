584880c1bbead5e5d778e705fe5c13a2f6d9940d
maany <imptodefeat@gmail.com>
2016-10-31 13:16:49 +0530

    OneToOne mapping between Encounter and MRRTReport

--- api/src/main/java/org/openmrs/module/radiologyfhirsupport/MRRTReport.java
+++ api/src/main/java/org/openmrs/module/radiologyfhirsupport/MRRTReport.java
@@ -23,7 +23,7 @@ public class MRRTReport extends BaseOpenmrsObject{
     private MRRTTemplate mrrtTemplate;
 
     @JoinColumn(name = "encounter")
-    @ManyToOne(fetch = FetchType.EAGER)
+    @OneToOne(fetch = FetchType.EAGER)
     public Encounter encounter;
 
     // TODO this field would be replaced by changesets in future implementations
